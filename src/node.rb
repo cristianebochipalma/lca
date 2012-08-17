a4789a184ba4df78a4c67971890b4aa698fc3117
Cristiane Bochi Palma <cristianebochipalma@gmail.com>
2012-08-16 21:06:28 -0300

    Right child
    Created a node test for the right child.
    
    Created code to right child.

--- src/node.rb
+++ src/node.rb
@@ -2,7 +2,7 @@ class Node
 
   attr_accessor :value
   attr_accessor :left_child
-  
+  attr_accessor :right_child
 
 end
 
--- test/test_node.rb
+++ test/test_node.rb
@@ -13,4 +13,9 @@ class TestNode < Test::Unit::TestCase
   	assert node.respond_to? :left_child
   end
 
+  def test_has_right_child
+  	node = Node.new
+  	assert node.respond_to? :right_child
+  end
+
 end
